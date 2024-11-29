9aa1629a4f6604eb7507d2461fa85c838308727a
Mansi agarwal <mansiagarwal293@gmail.com>
2024-11-29 14:21:39 +0530

    even function complete

---
+++ index.js
@@ -0,0 +1,11 @@
+function checkPrime(num) {
+  if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false; // If divisible by any number other than 1 and itself
+    }
+  }
+  return true; // The number is prime
+}
+
+console.log(checkPrime(5));
